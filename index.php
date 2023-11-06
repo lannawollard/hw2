5f6f10a4de72be5e70e4c5f61a4ebad8582f4509
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-11-06 14:13:06 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ $pageTitle= "Home";
 include "view-header.php";
 ?>
     <h1>Home</h1>
+<p id="demo" style="display:none">Hello JavaScript!</p>
+
+<button type="button" onclick="document.getElementById('demo').style.display='block'">Click Me!</button>
 <?php
 include "view-footer.php";
 ?>
