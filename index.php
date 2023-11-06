b9b8f03fcb1ad195837b4a21b6b84ebd9d9d7b10
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-11-06 14:13:48 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $pageTitle= "Home";
 include "view-header.php";
 ?>
     <h1>Home</h1>
-<p id="demo" style="display:none">Hello JavaScript!</p>
+<p id="demo" style="display:none">Hello! Welcome to Homework 5!</p>
 
 <button type="button" onclick="document.getElementById('demo').style.display='block'">Click Me!</button>
 <?php
