05d07bf0a227264bef8564980b32589ffc34ece8
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-11-06 14:14:57 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ include "view-header.php";
 <p id="demo" style="display:none">Hello! Welcome to Homework 5!</p>
 
 <button type="button" onclick="document.getElementById('demo').style.display='block'">Click Me!</button>
+<p id="demo">JavaScript can change HTML content--just watch!.</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Told you!"'>Click Me!</button>
 <?php
 include "view-footer.php";
 ?>
