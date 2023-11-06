37ed864ded150d3cba44bcc50be710c7e9ca1e82
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-11-06 14:28:21 -0600

    Update index.php

--- index.php
+++ index.php
@@ -22,6 +22,13 @@ include "view-header.php";
 
 <button type="button" onclick="document.getElementById('fourth').style.display='block'">Click Me!</button>
 
+<h5>last example</h5>
+<button onclick="document.getElementById('myImage').src='pic_bulbon.gif'">Turn on the light</button>
+
+<img id="myImage" src="pic_bulboff.gif" style="width:100px">
+
+<button onclick="document.getElementById('myImage').src='pic_bulboff.gif'">Turn off the light</button>
+
 
 <?php
 include "view-footer.php";
