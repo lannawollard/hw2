e80a567e6b18f0bc383b05d8a6e4315699642096
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-11-06 14:20:47 -0600

    Update index.php

--- index.php
+++ index.php
@@ -9,12 +9,15 @@ include "view-header.php";
 
 <h2>Java Script Examples</h2>
 <p id="second">JavaScript can change HTML content--just watch!.</p>
+<button type="button" onclick='document.getElementById("second").innerHTML = "Told you!"'>Click Me!</button>
 
 <h3>and another one</h3>
+<button onclick="document.getElementById('myImage').src='pic_bulbon.gif'">Turn on the light</button>
 
+<img id="myImage" src="pic_bulboff.gif" style="width:100px">
 
+<button onclick="document.getElementById('myImage').src='pic_bulboff.gif'">Turn off the light</button>
 
-<button type="button" onclick='document.getElementById("second").innerHTML = "Told you!"'>Click Me!</button>
 <?php
 include "view-footer.php";
 ?>
